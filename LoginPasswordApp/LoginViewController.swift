5d1668a97bf4191f16c504f791df154809d073a8
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2022-10-02 22:55:06 +0300

    The application is ready for testing!

--- LoginPasswordApp/LoginViewController.swift
+++ LoginPasswordApp/LoginViewController.swift
@@ -9,23 +9,28 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    // MARK: IBOutlets
     @IBOutlet var userNameTF: UITextField!
     @IBOutlet var passwordTF: UITextField!
     @IBOutlet var loginButton: UIButton!
 
+    // MARK: Private Properties
     private var userName = "User"
     private var password = "Password"
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+    // MARK: Life Cycles Methods
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
     }
-
+    
+    // MARK: Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
             guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
             welcomeVC.welcomeUser = userNameTF.text
     }
     
+    // MARK: IBActions
     @IBAction func loginButtonTapped() {
         if userNameTF.text != userName || passwordTF.text != password {
             showAlert(
@@ -55,11 +60,7 @@ class LoginViewController: UIViewController {
         passwordTF.text = ""
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesBegan(touches, with: event)
-        view.endEditing(true)
-    }
-    
+    // MARK: Private Methods
     private func showAlert(title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let okeyAction = UIAlertAction(title: "OK", style: .default)
