bbbccaa9ee658c7569b9158f665f4f60d9f763c5
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2022-10-02 17:02:47 +0300

    Implemented logic to check that the user and password are entered correctly, and that the user name is sent to the WelcomeViewController screen

--- LoginPasswordApp/LoginViewController.swift
+++ LoginPasswordApp/LoginViewController.swift
@@ -9,11 +9,64 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    @IBOutlet var loginButton: UIButton!
+
+    private var userName = "User"
+    private var password = "Password"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+            welcomeVC.welcomeUser = userNameTF.text
+    }
+    
+    @IBAction func loginButtonTapped() {
+        if userNameTF.text != userName || passwordTF.text != password {
+            showAlert(
+                title: "Invalid login or password!",
+                message: "Please, enter correct login and password."
+            )
+            passwordTF.text = ""
+        }
+    }
 
+    @IBAction func userNameForgotButtonTapped() {
+        showAlert(
+            title: "Hmmm...",
+            message: "Okey, your name - User"
+        )
+    }
+    
+    @IBAction func passwordForgotButtonTapped() {
+        showAlert(
+            title: "Hmmm...",
+            message: "Okey, your password - Password"
+        )
+    }
+    
+    @IBAction func unwind(segue: UIStoryboardSegue) {
+        userNameTF.text = ""
+        passwordTF.text = ""
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+    
 }
 
--- LoginPasswordApp/WelcomeViewController.swift
+++ LoginPasswordApp/WelcomeViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 
 final class WelcomeViewController: UIViewController {
 
+    @IBOutlet var welcomeLabel: UILabel!
+    
+    var welcomeUser: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        welcomeLabel.text = "Welcome, " + welcomeUser + "!"
+        
     }
 
     @IBAction func LogOutButtonDidTapped() {
